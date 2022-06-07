3a8a130c2552c7f46f093d341cb6640f3ba00d71
Keir Innes <keir.innes@gmail.com>
2022-06-07 17:50:24 +1200

    Updating latest changes

--- sign-up.js
+++ sign-up.js
@@ -1,17 +1,22 @@
 let newPassword = document.getElementById('password');
 let confirmPassword = document.getElementById('confirmPassword');
+let passwordLabel = document.getElementById('passwordLabel');
 
 newPassword.oninput = checkPasswords;
 confirmPassword.oninput = checkPasswords;
+
 
 checkPasswords();
 
 function checkPasswords(e) {
     if(newPassword.value != confirmPassword.value) {
-        console.log("They don't match");
         newPassword.classList.add('passwordNoMatch');
+        passwordLabel.classList.add('passwordLabel');
+        confirmPassword.classList.add('passwordNoMatch');
     } else {
-        console.log("They match!");
+        newPassword.classList.remove('passwordNoMatch');
+        confirmPassword.classList.remove('passwordNoMatch');
+        passwordLabel.classList.remove('passwordLabel');
     }
 }
 
